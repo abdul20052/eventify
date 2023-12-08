1877e4fe746df3a9fdaeeb809ea305e547c4f998
sparkk5 <abrardm101@gmail.com>
2023-12-08 18:20:31 +0800

    membuat halaman create event

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ app.secret_key = 'EVENTIFY'
 def home():
     return render_template('index.html')
 
+@app.route('/create_event')
+def create_event():
+    return render_template('create_event.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
