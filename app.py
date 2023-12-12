f3ab886c10a415eb2fcf8cb44079b054cc925cd1
sparkk5 <abrardm101@gmail.com>
2023-12-12 21:36:21 +0800

    update back-end halaman create_event

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, jsonify, request, session
 app = Flask(__name__)
 app.secret_key = 'EVENTIFY'
 
-# MONGODB_CONNECTION_STRING = "SECRET"
-# client = MongoClient(MONGODB_CONNECTION_STRING)
-# db = client.dblearningx
+MONGODB_CONNECTION_STRING = 'mongodb+srv://20051204052:l8YdBYlzNSheWHSf@unesa.sit4ohz.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(MONGODB_CONNECTION_STRING)
+db = client.db_eventify
 
 @app.route('/')
 def home():
@@ -26,7 +26,7 @@ def create_event():
     foto = request.files['foto']
 
     # Simpan data ke MongoDB
-    events_collection = mongo.db.events
+    events_collection = db.events
     data_event = {
         'event': event,
         'kategori': kategori,
